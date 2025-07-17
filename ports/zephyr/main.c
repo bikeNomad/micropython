aafeb3d6dcd1778118f96860b860ae93794861fe
Ned Konz <ned@metamagix.tech>
2025-07-17 14:46:34 -0700

    zephyr: Allow both /sd and /flash mounts if possible.
    Signed-off-by: Ned Konz <ned@metamagix.tech>

--- ports/zephyr/main.c
+++ ports/zephyr/main.c
@@ -96,12 +96,17 @@ void init_zephyr(void) {
 }
 
 #if MICROPY_VFS
+static int vfs_mount_and_chdir(const char *mount_point_str, qstr path_lib_qstr) {
+    mp_obj_t mount_point = mp_obj_new_str_from_cstr(mount_point_str);
+    int ret = mp_vfs_mount_and_chdir_protected(bdev, mount_point);
+    if (ret == 0) {
+        mp_obj_list_append(mp_sys_path, MP_OBJ_NEW_QSTR(path_lib_qstr));
+    }
+    return ret;
+}
+
 static void vfs_init(void) {
     mp_obj_t bdev = NULL;
-    mp_obj_t mount_point;
-    const char *mount_point_str = NULL;
-    qstr path_lib_qstr = MP_QSTRnull;
-    int ret = 0;
 
     #ifdef CONFIG_DISK_DRIVER_SDMMC
     #if KERNEL_VERSION_NUMBER >= ZEPHYR_VERSION(4, 0, 0)
@@ -110,19 +115,20 @@ static void vfs_init(void) {
     mp_obj_t args[] = { mp_obj_new_str_from_cstr(CONFIG_SDMMC_VOLUME_NAME) };
     #endif
     bdev = MP_OBJ_TYPE_GET_SLOT(&zephyr_disk_access_type, make_new)(&zephyr_disk_access_type, ARRAY_SIZE(args), 0, args);
-    mount_point_str = "/sd";
-    path_lib_qstr = MP_QSTR__slash_sd_slash_lib;
-    #elif defined(CONFIG_FLASH_MAP) && FIXED_PARTITION_EXISTS(storage_partition)
-    mp_obj_t args[] = { MP_OBJ_NEW_SMALL_INT(FIXED_PARTITION_ID(storage_partition)), MP_OBJ_NEW_SMALL_INT(4096) };
-    bdev = MP_OBJ_TYPE_GET_SLOT(&zephyr_flash_area_type, make_new)(&zephyr_flash_area_type, ARRAY_SIZE(args), 0, args);
-    mount_point_str = "/flash";
-    path_lib_qstr = MP_QSTR__slash_flash_slash_lib;
+    if ((bdev != NULL)) {
+        vfs_mount_and_chdir("/sd", MP_QSTR__slash_sd_slash_lib);
+    }
     #endif
 
+    #if defined(CONFIG_FLASH_MAP) && FIXED_PARTITION_EXISTS(storage_partition)
+    mp_obj_t args[] = { MP_OBJ_NEW_SMALL_INT(FIXED_PARTITION_ID(storage_partition)), MP_OBJ_NEW_SMALL_INT(4096) };
+    bdev = MP_OBJ_TYPE_GET_SLOT(&zephyr_flash_area_type, make_new)(&zephyr_flash_area_type, ARRAY_SIZE(args), 0, args);
     if ((bdev != NULL)) {
-        mount_point = mp_obj_new_str_from_cstr(mount_point_str);
-        ret = mp_vfs_mount_and_chdir_protected(bdev, mount_point);
-        mp_obj_list_append(mp_sys_path, MP_OBJ_NEW_QSTR(path_lib_qstr));
+        vfs_mount_and_chdir("/flash", MP_QSTR__slash_flash_slash_lib);
+    }
+    #endif
+
+    if (bdev == NULL) {
         // TODO: if this failed, make a new file system and try to mount again
     }
 }
