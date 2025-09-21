48adb1fc4262b7ae021b1345a7ad02b7970e308b
Ned Konz <ned@metamagix.tech>
2025-09-20 17:24:14 -0700

    zephyr: Finish xiao BLE NRF52840 integration.

--- ports/zephyr/modules/_boot.py
+++ ports/zephyr/modules/_boot.py
@@ -1,21 +1,36 @@
-import vfs
+import vfs, sys, os
+from zephyr import FlashArea
+from micropython import const
+
+_FLASH = const("/flash")
+_FLASH_LIB = const("/flash/lib")
+_STORAGE_KEY = const("id_storage")
 
 # Mount the partition labeled Storage on /flash
-mounts = vfs.mount()
-for mount in mounts:
-    if mount[1] == "/flash":
-        del vfs, mounts
-        return
+def mount_flash_partition():
+    mounts = vfs.mount()
+    for mount in mounts:
+        if mount[1] == _FLASH:
+            return True
+    return False
 
-from zephyr import FlashArea
+# Create an LFS2 filesystem on the partition labeled Storage
+# and mount it on /flash
+def create_flash_partition():
+    for key, value in FlashArea.__dict__.items():
+        if key.lower() == _STORAGE_KEY:
+            bdev = FlashArea(value, 4096)
+            try:
+                vfs.mount(bdev, _FLASH)
+            except OSError:
+                vfs.VfsLfs2.mkfs(bdev)
+                vfs.mount(bdev, _FLASH)
+            sys.path.append(_FLASH_LIB)
+            os.chdir(_FLASH)
 
-for key, value in FlashArea.__dict__.items():
-    if key.lower() == 'id_storage':
-        bdev = FlashArea(value, 4096)
-        try:
-            vfs.mount(bdev, "/flash")
-        except OSError:
-            vfs.VfsLfs2.mkfs(bdev)
-            vfs.mount(bdev, "/flash")
 
-del vfs, mounts, key, value, bdev, FlashArea
+if not mount_flash_partition():
+    create_flash_partition()
+
+del sys, vfs, os, const, FlashArea
+del mount_flash_partition, create_flash_partition
